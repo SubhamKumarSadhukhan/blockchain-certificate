de74e906832ca359410e2c66ba8978266ac39aa9
swagatacodes <sbhowmick50@gmail.com>
2023-08-28 01:52:09 +0530

    Home and About page modified

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -6,11 +6,11 @@ import { Web3Button } from '@web3modal/react'
 export default function index() {
   const { address,isConnected } = useAccount();
   return (
-    <div className="hero min-h-screen bg-secondary rounded-md">
+    <div className="hero min-h-screen ">
         <div className="hero-content flex-col lg:flex-row-reverse">
             <div>
-              <h1 className="text-5xl font-bold">This is an onchain blockchain certificate generator</h1>
-              <p className="py-6">Now days every Lms platform want to generate such certificate that can be persistent and easy to verify. So our SAAS will solve the problem by create a block chain baised certificate, here the data is even stored in block chain so it's completly transparrent to all users.</p>
+              <h1 className="text-5xl font-bold">An onchain Blockchain Certificate Generator</h1>
+              <p className="py-6">A user-friendly certificate generating software for every Lms platform. Transparancy at every step! </p>
               {isConnected?<Link href={`address/${address}/certificates`} className="btn">Check your certificates</Link>:<Web3Button/>}
             </div>
         </div>
--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -1,8 +1,18 @@
 
-export default function Avout() {
+export default function About() {
   return (
-    <div className="min-h-screen">
-      This is the About page
+    <div className="min-h-screen py-10 px-10">
+      <p className=" my-5 font-bold ...">About the software</p>
+      <p className="align-middle">Today, forging certificates has become a business tumbling from the need/want of
+        the people for employment. Graduates with legitimate certificates/degrees are denied
+        job opportunities by the holders of these forged credentials. To address this problem,
+        we have proposed a certificate verification system. Although the existing
+        systems can solve some of the major problems such as accessing student's records with the
+        provision of a central database to manage these records electronically. However, the system
+        can easily be hacked and manipulated since it is mostly available on centralized servers.
+        Blockchain is decentralized shared database so that no single person or group has control—instead,
+        all users collectively retain control.</p>
+
     </div>
   )
 }
