634948b0d6b63cc5b36204d1047589cf8c6dc296
swagatacodes <sbhowmick50@gmail.com>
2023-08-28 12:10:43 +0530

    About page modified

--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -1,17 +1,28 @@
-
+import './index.css';
 export default function About() {
   return (
     <div className="min-h-screen py-10 px-10">
       <p className=" my-5 font-bold ...">About the software</p>
-      <p className="align-middle">Today, forging certificates has become a business tumbling from the need/want of
-        the people for employment. Graduates with legitimate certificates/degrees are denied
-        job opportunities by the holders of these forged credentials. To address this problem,
-        we have proposed a certificate verification system. Although the existing
-        systems can solve some of the major problems such as accessing student's records with the
-        provision of a central database to manage these records electronically. However, the system
-        can easily be hacked and manipulated since it is mostly available on centralized servers.
-        Blockchain is decentralized shared database so that no single person or group has control—instead,
-        all users collectively retain control.</p>
+      <div >
+        <p className="para">Today, forging certificates has become a business tumbling from the need/want of
+          the people for employment. Graduates with legitimate certificates/degrees are denied
+          job opportunities by the holders of these forged credentials. To address this problem,
+          we have proposed a certificate verification system. Although the existing
+          systems can solve some of the major problems such as accessing student's records with the
+          provision of a central database to manage these records electronically. However, the system
+          can easily be hacked and manipulated since it is mostly available on centralized servers.
+          Blockchain is decentralized shared database so that no single person or group has control—instead,
+          all users collectively retain control.<br/>
+          With a unique approach, we not only help you create stunning certificates but also facilitate the entire process.
+          Simply input the event details along with the respective year.
+          Our seamless system will securely transmit your information to the relevant board. Once verified, the board will generate
+          your certificate and post it directly to your user profile.<br/>
+          We take pride in streamlining this process, ensuring accuracy, security, and convenience. Join us in celebrating your
+          academic milestones while we handle the technicalities, making your certificate procurement experience smooth and rewarding.
+          Your achievements deserve to be showcased, and we're here to make it happen!
+        </p>
+      </div>
+
 
     </div>
   )
